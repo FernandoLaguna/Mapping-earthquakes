5b2331506fff1549366eaa00ab2c86944ebe1b6c
lourdes dergal <fer_laguna@yahoo.com>
2021-12-08 19:14:57 -0600

    mapping multiple markers

---
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -0,0 +1,42 @@
+// Add console.log to check to see if our code is working.
+console.log("workings");
+
+// Create the map object with a center and zoom level. The mapid will reference the id tag in our <div> element on the index.html file
+// The setView() method sets the view of the map with a geographical center, where the first coordinate is latitude (40.7) and the second is longitude (-94.5). 
+// We set the zoom level of "4" on a scale 0–18.
+let map = L.map('mapid').setView([34.0522, -118.2437], 4);
+
+// Get data from cities.js
+let cityData = cities;
+
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population/100000
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString()  + "</h3>")
+  .addTo(map);
+});
+
+
+
+// We create the tile layer that will be the background of our map.
+// assign the tileLayer() method  to the variable streets. Leaflet offers various tile layer APIs
+// We add the id attribute and assign it mapbox/streets-v11, which will show the streets on the map. mapbox/streets-v11, pueden sr:
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    Sattribution: 'Map data &copy <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
+
+
+
+
+
+
